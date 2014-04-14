e039e66f966219de0c4179c50a8a894ff83a6042
JulissaJ <julissa.jansen@gmail.com>
2014-04-14 12:01:33 -0400

    fixed typo

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Manufacturer do
 
   it {should have_valid(:name).when('Tesla') }
-  it {shoud_note have_valid(:name).when(nil, '')}
+  it {should_not have_valid(:name).when(nil, '')}
 
   it {should have_valid(:country).when('England') }
   it {should_not have_valid(:country).when(nil, '') }
